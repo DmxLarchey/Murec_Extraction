968ec05d6550c7004ea66388a140e99d70e5b17c
Dominique Larchey-Wendling <larchey@gmail.com>
2023-02-22 15:43:54 +0100

    oublié de save

--- theories/map_compute.v
+++ theories/map_compute.v
@@ -13,7 +13,7 @@ From Coq Require Import Utf8.
 
 From MuRec Require Import sigma relations index vec computable_def.
 
-Section vec_dmap.
+Section vec_map_compute.
 
   Variables (X Y : Type)
             (F : X → Y → Prop)
@@ -21,31 +21,31 @@ Section vec_dmap.
 
   Section vec_map_compute_props.
 
-    Local Fact vdm_PO1 i : F ⟨⟩.[i] ⟨⟩.[i].
+    Local Fact vmc_PO1 i : F ⟨⟩.[i] ⟨⟩.[i].
     Proof. destruct (idx_inv i). Qed.
 
     Variables (x : X) (n : nat) (v : vec X n) (y : Y) (w : vec Y n)
               (Fx : F x y)
               (Fv : ∀ i, F v.[i] w.[i]).
 
-    Local Fact vdm_PO2 i : F (x ∷ v).[i] (y ∷ w).[i].
+    Local Fact vmc_PO2 i : F (x ∷ v).[i] (y ∷ w).[i].
     Proof. now destruct (idx_inv i); cbn. Qed.
 
   End vec_map_compute_props.
 
-  Arguments vdm_PO2 {_ _ _ _ _}.
+  Arguments vmc_PO2 {_ _ _ _ _}.
 
   Definition vec_map_compute : ∀{n} (v : vec X n), computable (λ w, ∀i, F v.[i] w.[i]) :=
     let fix loop {n} (v : vec X n) : (∀i, ex (F v.[i])) → _ :=
       match v with
-      | ⟨⟩    => λ _,   ⟪⟨⟩, vdm_PO1⟫
+      | ⟨⟩    => λ _,   ⟪⟨⟩, vmc_PO1⟫
       | x ∷ v => λ hxv, let (y, hy) := f x (hxv 𝕆) in
                         let (w, hw) := loop v (λ i, hxv (𝕊 i)) in
-                        ⟪y ∷ w, vdm_PO2 hy hw⟫
+                        ⟪y ∷ w, vmc_PO2 hy hw⟫
       end in
     λ n v hv, loop v (λ i, let (w, hw) := hv in ⟪w.[i], hw i⟫ₚ).
 
-End vec_dmap.
+End vec_map_compute.
 
 Arguments vec_map_compute {_ _ _} _ {n} v.
 
